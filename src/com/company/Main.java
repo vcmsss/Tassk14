30b01803daa7c3677a96391b732cdfb4776aea9d
vcmsss <pak.al.ig@yandex.ru>
2019-07-27 20:03:19 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+                Scanner in = new Scanner(System.in);
+                System.out.print("Input a number: ");
+                String S = in.nextLine();
+                System.out.println("String: " + S);
+
+                int X = Integer.parseInt (S);
+                System.out.println("Int: " + X);
+
+                byte Y = (byte) X;
+                System.out.println("Byte: " + Y);
+            }
+        }
